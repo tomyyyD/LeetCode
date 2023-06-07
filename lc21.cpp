9b7c2c26596dcff1c00bfcc4824638f92b9cdf25
tomyyyD <thomas5d@comcast.net>
2023-06-07 11:20:38 -0400

    formatting and mergeTwoLists function

--- lc21.cpp
+++ lc21.cpp
@@ -1,7 +1,8 @@
 #include <cstdlib>
 #include <stdio.h>
 
-struct ListNode {
+struct ListNode
+{
     // contains
     int val;
     ListNode *next;
@@ -11,10 +12,48 @@ struct ListNode {
     ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
-// ListNode* mergeTwoLists(ListNode* list1, ListNode* list2)
-// {
+ListNode *mergeTwoLists(ListNode *list1, ListNode *list2)
+{
+    ListNode *head1 = list1;
+    ListNode *head2 = list2;
+    ListNode *lastchecked = list2;
+    if (head2 == nullptr)
+        return head1;
+    if (head1 == nullptr)
+        return head2;
+    ListNode *start = (head1->val <= head2->val) ? head1 : head2;
+    while (head1 != nullptr)
+    {
+        if (head1->val <= head2->val)
+        {
+            if (lastchecked != nullptr)
+                lastchecked->next = head1;
+            lastchecked = head1;
+            head1 = head1->next;
+            lastchecked->next = head2;
+        }
+        else
+        {
+            if (head2->next == nullptr)
+            {
+                // getting here means least element in the list made by head1 is
+                // greater than greatest element in the current made by head2.
+                head2->next = head1;
+                break;
+            }
+            else
+            {
+                head2 = head2->next;
+                lastchecked = head2;
+            }
+        }
+    }
+}
 
-// }
+/*
+Testing functions
+================================================================================
+*/
 
 int increaseByOne(int x)
 {
@@ -27,23 +66,34 @@ int sqaure(int x)
 }
 
 // function to create the lists
-ListNode* makeList(int length, int(*func)(int))
+ListNode *makeList(int length, int (*func)(int))
 {
     ListNode *header = (ListNode *)malloc(sizeof(ListNode));
-    if (length == 0) return header;
+    if (length == 0)
+        return header;
 
-    header -> val = (* func) (0);
-    header -> next = nullptr;
+    header->val = (*func)(0);
+    header->next = nullptr;
     ListNode *a = header;
 
-    for (int i = 1; i < length; i ++)
+    for (int i = 1; i < length; i++)
     {
         ListNode *b = (ListNode *)malloc(sizeof(ListNode));
-        b -> val = (* func)(i);
-        a -> next = b;
+        b->val = (*func)(i);
+        a->next = b;
         a = b;
     }
     return header;
+}
+
+void printList(ListNode *list)
+{
+    // print nullpty terminated list
+    while (list != nullptr)
+    {
+        printf("%d", list->val);
+        list = list->next;
+    }
 }
 
 int main()
@@ -55,19 +105,23 @@ int main()
 
     ListNode *l1 = makeList(5, f);
     ListNode *l2 = makeList(5, g);
-    while (l1 != nullptr){
-        printf("%d, ", l1 -> val);
-        ListNode *temp = l1 -> next;
+    while (l1 != nullptr)
+    {
+        printf("%d, ", l1->val);
+        ListNode *temp = l1->next;
         free(l1);
         l1 = temp;
     }
     printf("\n");
-    while (l2 != nullptr){
-        printf("%d, ", l2 -> val);
-        ListNode *temp = l2 -> next;
+    while (l2 != nullptr)
+    {
+        printf("%d, ", l2->val);
+        ListNode *temp = l2->next;
         free(l2);
         l2 = temp;
     }
+
+    ListNode *merged = mergeTwoLists(l1, l2);
     printf("\n");
     return 0;
 }
